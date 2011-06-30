5f776d48e0515d968e976e50262f88d04b8751a3
James Harton <jamesotron@gmail.com>
2011-06-30 17:07:01 +1200

    Add IPTulple and IPProtocol classes.

--- lib/ipaddr_extensions.rb
+++ lib/ipaddr_extensions.rb
@@ -400,6 +400,7 @@ module Sociable
     end
 
   end
+
   module StringIPExtensions
     def to_ip
       begin
@@ -424,6 +425,201 @@ module Sociable
       end
       IPAddr.new(self.to_i, af)
     end
+  end
+end
+
+class IPTuple
+
+  attr_accessor :source_ip, :destination_ip, :ip_protocol, :source_port, :destination_port
+
+  def initialize src_ip=nil, dst_ip=nil, ip_proto=nil, src_port=nil, dst_port=nil
+    @source_ip = src_ip if src_ip.is_a? IPAddr
+    @source_ip = src_ip.to_ip if src_ip.is_a? String
+    @destination_ip = dst_ip if dst_ip.is_a? IPAddr
+    @destination_ip = dst_ip.to_ip if dst_ip.is_a? String
+    @ip_protocol = ip_proto if ip_proto.is_a? IPProtocol
+    @ip_protocol = IPProtocol.new(ip_proto) if ip_proto.is_a? Integer
+    @source_port = src_port if ((1..65535).include? src_port)
+    @destination_port = dst_port if ((1..65535).include? dst_port)
+  end
+
+  def ipv4?
+    if @source_ip && @destination_ip
+      @source_ip.ipv4? && @destination_ip.ipv4?
+    elsif @source_ip
+      @source_ip.ipv4?
+    elsif @destination_ip
+      @destination_ip.ipv4?
+    end
+  end
+
+  def ipv6?
+    if @source_ip && @destination_ip
+      @source_ip.ipv6? && @destination_ip.ipv6?
+    elsif @source_ip
+      @source_ip.ipv6?
+    elsif @destination_ip
+      @destination_ip.ipv6?
+    end
+  end
+end
+
+class IPProtocol
+  # See http://www.iana.org/assignments/protocol-numbers/protocol-numbers.txt
+  NUMBERS = (1..142).to_a + [ 255 ]
+  NAMES = {
+    0 => "HOPOPT",
+    1 => "ICMP",
+    2 => "IGMP",
+    3 => "GGP",
+    4 => "IPv4",
+    5 => "ST",
+    6 => "TCP",
+    7 => "CBT",
+    8 => "EGP",
+    9 => "IGP",
+    10 => "BBN-RCC-MON",
+    11 => "NVP-II",
+    12 => "PUP",
+    13 => "ARGUS",
+    14 => "EMCON",
+    15 => "XNET",
+    16 => "CHAOS",
+    17 => "UDP",
+    18 => "MUX",
+    19 => "DCN-MEAS",
+    20 => "HMP",
+    21 => "PRM",
+    22 => "XNS-IDP",
+    23 => "TRUNK-1",
+    24 => "TRUNK-2",
+    25 => "LEAF-1",
+    26 => "LEAF-2",
+    27 => "RDP",
+    28 => "IRTP",
+    29 => "ISO-TP4",
+    30 => "NETBLT",
+    31 => "MFE-NSP",
+    32 => "MERIT-INP",
+    33 => "DCCP",
+    34 => "3PC",
+    35 => "IDPR",
+    36 => "XTP",
+    37 => "DDP",
+    38 => "IDPR-CMTP",
+    39 => "TP++",
+    40 => "IL",
+    41 => "IPv6",
+    42 => "SDRP",
+    43 => "IPv6-Route",
+    44 => "IPv6-Frag",
+    45 => "IDRP",
+    46 => "RSVP",
+    47 => "GRE",
+    48 => "DSR",
+    49 => "BNA",
+    50 => "ESP",
+    51 => "AH",
+    52 => "I-NLSP",
+    53 => "SWIPE",
+    54 => "NARP",
+    55 => "MOBILE",
+    56 => "TLSP",
+    57 => "SKIP",
+    58 => "IPv6-ICMP",
+    59 => "IPv6-NoNxt",
+    60 => "IPv6-Opts",
+    62 => "CFTP",
+    64 => "SAT-EXPAK",
+    65 => "KRYPTOLAN",
+    66 => "RVD",
+    67 => "IPPC",
+    69 => "SAT-MON",
+    70 => "VISA",
+    71 => "IPCV",
+    72 => "CPNX",
+    73 => "CPHB",
+    74 => "WSN",
+    75 => "PVP",
+    76 => "BR-SAT-MON",
+    77 => "SUN-ND",
+    78 => "WB-MON",
+    79 => "WB-EXPAK",
+    80 => "ISO-IP",
+    81 => "VMTP",
+    82 => "SECURE-VMTP",
+    83 => "VINES",
+    84 => "TTP",
+    84 => "IPTM",
+    85 => "NSFNET-IGP",
+    86 => "DGP",
+    87 => "TCF",
+    88 => "EIGRP",
+    89 => "OSPFIGP",
+    90 => "Sprite-RPC",
+    91 => "LARP",
+    92 => "MTP",
+    93 => "AX.25",
+    94 => "IPIP",
+    95 => "MICP",
+    96 => "SCC-SP",
+    97 => "ETHERIP",
+    98 => "ENCAP",
+    100 => "GMTP",
+    101 => "IFMP",
+    102 => "PNNI",
+    103 => "PIM",
+    104 => "ARIS",
+    105 => "SCPS",
+    106 => "QNX",
+    107 => "A/N",
+    108 => "IPComp",
+    109 => "SNP",
+    110 => "Compaq-Peer",
+    111 => "IPX-in-IP",
+    112 => "VRRP",
+    113 => "PGM",
+    115 => "L2TP",
+    116 => "DDX",
+    117 => "IATP",
+    118 => "STP",
+    119 => "SRP",
+    120 => "UTI",
+    121 => "SMP",
+    122 => "SM",
+    123 => "PTP",
+    124 => "ISIS over IPv4",
+    125 => "FIRE",
+    126 => "CRTP",
+    127 => "CRUDP",
+    128 => "SSCOPMCE",
+    129 => "IPLT",
+    130 => "SPS",
+    131 => "PIPE",
+    132 => "SCTP",
+    133 => "FC",
+    134 => "RSVP-E2E-IGNORE",
+    135 => "Mobility Header",
+    136 => "UDPLite",
+    137 => "MPLS-in-IP",
+    138 => "manet",
+    139 => "HIP",
+    140 => "Shim6",
+    141 => "WESP",
+    142 => "ROHC",
+    255 => "Reserved",
+  }
+
+  def initialize proto_num
+    @number = proto_num if NUMBERS.member? proto_num
+  end
+
+  def to_ip
+    @number
+  end
+
+  def to_s
+    NAMES[@number]
   end
 end
 
